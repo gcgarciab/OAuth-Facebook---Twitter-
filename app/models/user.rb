dd320a24907ad53702ce5f472f028da678422692
Camilo Garcia <gcgarciab@unal.edu.co>
2016-08-24 00:08:46 -0500

    Prueba Autenticación Facebook - Twiter  (Devise)

---
+++ app/models/user.rb
@@ -0,0 +1,62 @@
+  class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:twitter, :facebook]
+ 
+  def self.find_for_oauth(auth, signed_in_resource = nil)
+    identity = Identity.find_for_oauth(auth)
+    user = signed_in_resource ? signed_in_resource : identity.user
+ 
+    if user.nil?
+      email = auth.info.email
+      user = User.find_by(email: email) if email
+ 
+      # Create the user if it's a new registration
+      if user.nil?
+        password = Devise.friendly_token[0,20]
+        if auth.provider == 'facebook'
+          user = User.new(
+            email: email ? email : "#{auth.uid}@change-me.com",
+            password: password,
+            password_confirmation: password
+          )
+        elsif auth.provider == 'twitter'
+          user = User.new(
+            email: "#{auth.uid}@change-me.com",
+            password: password,
+            password_confirmation: password
+          )
+        end
+      end
+      user.save!
+    end
+ 
+    if identity.user != user
+      identity.user = user
+      identity.save!
+    end
+    
+    user
+  end
+ 
+  def email_verified?
+    if self.email
+      if self.email.split('@')[1] == 'change-me.com'
+        return false
+      else
+        return true
+      end
+    else
+      return false
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+end
